48598cdaf612baf7c8224908ffe41704ef6e6355
Ameringkseh <raflimramadhan.204@gmail.com>
2025-11-24 16:21:47 +0700

    el

---
+++ Pertemuan07/Praktikum/P7_1_714240059/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace P7_1_714240059
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.textBoxKelas = new System.Windows.Forms.TextBox();
+            this.comboBoxAngkatan = new System.Windows.Forms.ComboBox();
+            this.radioButtonWeekday = new System.Windows.Forms.RadioButton();
+            this.radioButtonWeekend = new System.Windows.Forms.RadioButton();
+            this.checkBoxKuliah = new System.Windows.Forms.CheckBox();
+            this.checkBoxTidur = new System.Windows.Forms.CheckBox();
+            this.checkBoxLiburan = new System.Windows.Forms.CheckBox();
+            this.buttonCek = new System.Windows.Forms.Button();
+            this.buttonTutup = new System.Windows.Forms.Button();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelKegiatan = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(88, 25);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(158, 23);
+            this.textBoxNama.TabIndex = 0;
+            // 
+            // textBoxKelas
+            // 
+            this.textBoxKelas.Location = new System.Drawing.Point(88, 107);
+            this.textBoxKelas.Multiline = true;
+            this.textBoxKelas.Name = "textBoxKelas";
+            this.textBoxKelas.Size = new System.Drawing.Size(158, 67);
+            this.textBoxKelas.TabIndex = 1;
+            // 
+            // comboBoxAngkatan
+            // 
+            this.comboBoxAngkatan.FormattingEnabled = true;
+            this.comboBoxAngkatan.Items.AddRange(new object[] {
+            "2020",
+            "2021",
+            "2022",
+            "2023",
+            "2024",
+            "2025"});
+            this.comboBoxAngkatan.Location = new System.Drawing.Point(88, 64);
+            this.comboBoxAngkatan.Name = "comboBoxAngkatan";
+            this.comboBoxAngkatan.Size = new System.Drawing.Size(158, 26);
+            this.comboBoxAngkatan.TabIndex = 2;
+            // 
+            // radioButtonWeekday
+            // 
+            this.radioButtonWeekday.AutoSize = true;
+            this.radioButtonWeekday.Location = new System.Drawing.Point(25, 273);
+            this.radioButtonWeekday.Name = "radioButtonWeekday";
+            this.radioButtonWeekday.Size = new System.Drawing.Size(112, 22);
+            this.radioButtonWeekday.TabIndex = 3;
+            this.radioButtonWeekday.TabStop = true;
+            this.radioButtonWeekday.Text = "Senin - Jum\'at";
+            this.radioButtonWeekday.UseVisualStyleBackColor = true;
+            this.radioButtonWeekday.CheckedChanged += new System.EventHandler(this.radioButtonWeekday_CheckedChanged);
+            // 
+            // radioButtonWeekend
+            // 
+            this.radioButtonWeekend.AutoSize = true;
+            this.radioButtonWeekend.Location = new System.Drawing.Point(25, 301);
+            this.radioButtonWeekend.Name = "radioButtonWeekend";
+            this.radioButtonWeekend.Size = new System.Drawing.Size(117, 22);
+            this.radioButtonWeekend.TabIndex = 4;
+            this.radioButtonWeekend.TabStop = true;
+            this.radioButtonWeekend.Text = "Sabtu - Minggu";
+            this.radioButtonWeekend.UseVisualStyleBackColor = true;
+            this.radioButtonWeekend.CheckedChanged += new System.EventHandler(this.radioButtonWeekend_CheckedChanged);
+            // 
+            // checkBoxKuliah
+            // 
+            this.checkBoxKuliah.AutoSize = true;
+            this.checkBoxKuliah.Location = new System.Drawing.Point(25, 343);
+            this.checkBoxKuliah.Name = "checkBoxKuliah";
+            this.checkBoxKuliah.Size = new System.Drawing.Size(65, 22);
+            this.checkBoxKuliah.TabIndex = 5;
+            this.checkBoxKuliah.Text = "Kuliah";
+            this.checkBoxKuliah.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTidur
+            // 
+            this.checkBoxTidur.AutoSize = true;
+            this.checkBoxTidur.Location = new System.Drawing.Point(25, 371);
+            this.checkBoxTidur.Name = "checkBoxTidur";
+            this.checkBoxTidur.Size = new System.Drawing.Size(60, 22);
+            this.checkBoxTidur.TabIndex = 6;
+            this.checkBoxTidur.Text = "Tidur";
+            this.checkBoxTidur.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxLiburan
+            // 
+            this.checkBoxLiburan.AutoSize = true;
+            this.checkBoxLiburan.Location = new System.Drawing.Point(25, 399);
+            this.checkBoxLiburan.Name = "checkBoxLiburan";
+            this.checkBoxLiburan.Size = new System.Drawing.Size(74, 22);
+            this.checkBoxLiburan.TabIndex = 7;
+            this.checkBoxLiburan.Text = "Liburan";
+            this.checkBoxLiburan.UseVisualStyleBackColor = true;
+            // 
+            // buttonCek
+            // 
+            this.buttonCek.Location = new System.Drawing.Point(88, 191);
+            this.buttonCek.Name = "buttonCek";
+            this.buttonCek.Size = new System.Drawing.Size(66, 26);
+            this.buttonCek.TabIndex = 8;
+            this.buttonCek.Text = "Cek";
+            this.buttonCek.UseVisualStyleBackColor = true;
+            this.buttonCek.Click += new System.EventHandler(this.buttonCek_Click);
+            // 
+            // buttonTutup
+            // 
+            this.buttonTutup.Location = new System.Drawing.Point(179, 191);
+            this.buttonTutup.Name = "buttonTutup";
+            this.buttonTutup.Size = new System.Drawing.Size(66, 26);
+            this.buttonTutup.TabIndex = 9;
+            this.buttonTutup.Text = "Tutup";
+            this.buttonTutup.UseVisualStyleBackColor = true;
+            this.buttonTutup.Click += new System.EventHandler(this.buttonTutup_Click);
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Location = new System.Drawing.Point(88, 451);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(66, 27);
+            this.buttonPrint.TabIndex = 10;
+            this.buttonPrint.Text = "Print";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(180, 451);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(66, 27);
+            this.buttonReset.TabIndex = 11;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 18);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 18);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Angkatan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 18);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Kelas";
+            // 
+            // labelKegiatan
+            // 
+            this.labelKegiatan.AutoSize = true;
+            this.labelKegiatan.Font = new System.Drawing.Font("Trebuchet MS", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKegiatan.Location = new System.Drawing.Point(119, 241);
+            this.labelKegiatan.Name = "labelKegiatan";
+            this.labelKegiatan.Size = new System.Drawing.Size(86, 23);
+            this.labelKegiatan.TabIndex = 15;
+            this.labelKegiatan.Text = "KEGIATAN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 543);
+            this.Controls.Add(this.labelKegiatan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.buttonTutup);
+            this.Controls.Add(this.buttonCek);
+            this.Controls.Add(this.checkBoxLiburan);
+            this.Controls.Add(this.checkBoxTidur);
+            this.Controls.Add(this.checkBoxKuliah);
+            this.Controls.Add(this.radioButtonWeekend);
+            this.Controls.Add(this.radioButtonWeekday);
+            this.Controls.Add(this.comboBoxAngkatan);
+            this.Controls.Add(this.textBoxKelas);
+            this.Controls.Add(this.textBoxNama);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.TextBox textBoxKelas;
+        private System.Windows.Forms.ComboBox comboBoxAngkatan;
+        private System.Windows.Forms.RadioButton radioButtonWeekday;
+        private System.Windows.Forms.RadioButton radioButtonWeekend;
+        private System.Windows.Forms.CheckBox checkBoxKuliah;
+        private System.Windows.Forms.CheckBox checkBoxTidur;
+        private System.Windows.Forms.CheckBox checkBoxLiburan;
+        private System.Windows.Forms.Button buttonCek;
+        private System.Windows.Forms.Button buttonTutup;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelKegiatan;
+    }
+}
+
