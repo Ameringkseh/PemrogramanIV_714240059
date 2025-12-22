3e2ebb1a53cf0c5d24a0f6aac925917ac79599b0
Ameringkseh <raflimramadhan.204@gmail.com>
2025-12-22 15:03:04 +0700

    gas

---
+++ Pertemuan11/Praktikum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using P9_714240059.view;
+
+namespace P9_714240059
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ParentForm());
+        }
+    }
+}
